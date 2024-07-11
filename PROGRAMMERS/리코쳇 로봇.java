677f803a1e7a2508ddac74893f21a520c8aff754
임하은 <73926352+kodms08@users.noreply.github.com>
2024-07-11 17:54:00 +0900

    Java / 27.518888888888885ms

--- PROGRAMMERS/리코쳇 로봇.java
+++ PROGRAMMERS/리코쳇 로봇.java
@@ -1,35 +1,4 @@
-import java.util.*;
-​
-class Solution {
-    public int solution(String[] board) {
-        int answer = -1;
-        
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
-        
-        int[] robot = new int[2];
-        char[][] map = new char[board.length][board[0].length()];
-        for(int i=0; i<board.length; i++) {
-            for(int j=0; j<board[i].length(); j++) {
-                map[i][j] = board[i].charAt(j);
-                if(map[i][j] == 'R') robot = new int[] {i,j};
-            }
-        }
-        
-        int[][] visited = new int[board.length][board[0].length()];
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(robot);
-        visited[robot[0]][robot[1]] = 1;
-        
-        while(!queue.isEmpty()) {
-            int[] cur =  queue.poll();
-            
-            for(int d=0; d<4; d++) {
-                int cx = cur[0];
-                int cy = cur[1];
-                while(cx+dx[d]>=0 && cx+dx[d]<board.length &&
-                    cy+dy[d]>=0 && cy+dy[d]<board[0].length()) {
-                    
+}
                     cx+=dx[d];
                     cy+=dy[d];
                     if(map[cx][cy]=='D') {
@@ -53,16 +22,9 @@ class Solution {
                         }
                         break;
                     }
-​
-                    
                 }
             }
-                
-​
-            
         }
-        
-        
         
         return answer;
     }

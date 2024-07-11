19ca1a58d7801043ccf8bdf47318aa74bb810456
임하은 <73926352+kodms08@users.noreply.github.com>
2024-07-11 16:58:44 +0900

    Java / 30.20148148148148ms

--- PROGRAMMERS/리코쳇 로봇.java
+++ PROGRAMMERS/리코쳇 로봇.java
@@ -40,3 +40,32 @@ class Solution {
                             visited[cx-dx[d]][cy-dy[d]] = visited[cur[0]][cur[1]]+1;
                             if(map[cx-dx[d]][cy-dy[d]] == 'G') {
                                 answer = Integer.max(answer,visited[cur[0]][cur[1]]);
+                            }
+                        }
+                        break;
+​
+                    }else if(cx+dx[d]<0 || cy+dy[d]<0 || 
+                             cx+dx[d]>=board.length || cy+dy[d]>=board[0].length()) {
+                        if(visited[cx][cy]!=0) break;
+                        queue.add(new int[] {cx,cy});
+                        System.out.printf("%d %d\n",cx,cy);
+                        visited[cx][cy] = visited[cur[0]][cur[1]]+1;
+                        if(map[cx][cy] == 'G') {
+                            answer = Integer.max(answer,visited[cur[0]][cur[1]]);
+                        }
+                        break;
+                    }
+​
+                    
+                }
+            }
+                
+​
+            
+        }
+        
+        
+        
+        return answer;
+    }
+}

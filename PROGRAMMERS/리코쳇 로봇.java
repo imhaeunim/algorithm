6a9c9b5089a34f2dcf3a39c38baa706597255922
임하은 <73926352+kodms08@users.noreply.github.com>
2024-07-11 16:49:56 +0900

    Java / 35.828148148148145ms

--- PROGRAMMERS/리코쳇 로봇.java
+++ PROGRAMMERS/리코쳇 로봇.java
@@ -1,3 +1,26 @@
+import java.util.*;
+import java.io.*;
+​
+class Solution {
+    public int solution(String[] board) {
+        int answer = -1;
+        
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
+        
+        int[] robot = new int[2];
+        char[][] map = new char[board.length][board[0].length()];
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[i].length(); j++) {
+                map[i][j] = board[i].charAt(j);
+                if(map[i][j] == 'R') robot = new int[] {i,j};
+            }
+        }
+        
+        int[][] visited = new int[board.length][board[0].length()];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(robot);
+        visited[robot[0]][robot[1]] = 1;
         
         while(!queue.isEmpty()) {
             int[] cur =  queue.poll();
@@ -43,3 +66,8 @@
             
         }
         
+        
+        
+        return answer;
+    }
+}

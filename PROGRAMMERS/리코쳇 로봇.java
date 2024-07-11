8fcf18fa74987c4a4188a56c59a89a9ce664e430
임하은 <73926352+kodms08@users.noreply.github.com>
2024-07-11 16:53:47 +0900

    Java / 36.10259259259259ms

--- PROGRAMMERS/리코쳇 로봇.java
+++ PROGRAMMERS/리코쳇 로봇.java
@@ -25,7 +25,6 @@ class Solution {
         while(!queue.isEmpty()) {
             int[] cur =  queue.poll();
             System.out.println(Arrays.toString(cur));
-            // System.out.println(visited[cur[0]][cur[1]]);
             
             for(int d=0; d<4; d++) {
                 int cx = cur[0];
@@ -40,34 +39,3 @@ class Solution {
                             System.out.printf("%d %d\n",cx-dx[d],cy-dy[d]);
                             queue.add(new int[] {cx-dx[d],cy-dy[d]});
                             visited[cx-dx[d]][cy-dy[d]] = visited[cur[0]][cur[1]]+1;
-                            if(map[cx-dx[d]][cy-dy[d]] == 'G') {
-                                answer = Integer.max(answer,visited[cur[0]][cur[1]]);
-                            }
-                        }
-                        break;
-​
-                    }else if(cx+dx[d]<0 || cy+dy[d]<0 || 
-                             cx+dx[d]>=board.length || cy+dy[d]>=board[0].length()) {
-                        if(visited[cx][cy]!=0) break;
-                        queue.add(new int[] {cx,cy});
-                        System.out.printf("%d %d\n",cx,cy);
-                        visited[cx][cy] = visited[cur[0]][cur[1]]+1;
-                        if(map[cx][cy] == 'G') {
-                            answer = Integer.max(answer,visited[cur[0]][cur[1]]);
-                        }
-                        break;
-                    }
-​
-                    
-                }
-            }
-                
-​
-            
-        }
-        
-        
-        
-        return answer;
-    }
-}

327050efb7bacdb9924e46a2fae09d4e05a56cc3
임하은 <73926352+kodms08@users.noreply.github.com>
2024-10-21 23:58:21 +0900

    Java / 11.879523809523809ms

---
+++ PROGRAMMERS/340212_[PCCP 기출문제] 2번 / 퍼즐 게임 챌린지.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] diffs, int[] times, long limit) {
+        int answer = 0;
+        
+        int start = 0;
+        int end = Integer.MAX_VALUE;
+        
+        while(start<end) {
+            int mid = (end+start)/2;
+            
+            long c = calculate(mid, diffs, times);
+            if(c>limit) {
+                start = mid+1;
+            }else {
+                end = mid;
+            }
+        }
+        
+        end = end==0?1:end;
+        return end;
+    }
+    
+    static long calculate(int level, int[] diffs, int[] times) {
+        long sum = 0;
+        for(int i=0; i<diffs.length; i++) {
+            if(diffs[i]<=level) sum+=times[i];
+            else{
+                int time_prev = i-1<0? 0: times[i-1];
+                sum+=(diffs[i]-level)*(times[i]+time_prev)+times[i];
+            }
+        }
+        return sum;
+    }
+}

21a0c7d229f5754f7306de58db7b5166a5f56d62
Limhaeun <73926352+imhaeunim@users.noreply.github.com>
2025-01-07 12:42:40 +0900

    Java / 0.6906249999999999ms

--- PROGRAMMERS/42747_H-Index.java
+++ PROGRAMMERS/42747_H-Index.java
@@ -6,21 +6,27 @@ class Solution {
         
         Arrays.sort(citations);
         
-        int left = 0;
-        int right = citations.length-1;
+//         int left = 0;
+//         int right = citations.length-1;
         
-        while(left<=right) {
-            int mid = (right+left)/2;
+//         while(left<=right) {
+//             int mid = (right+left)/2;
             
-            if(citations[mid]>=citations.length-mid) {
-                right = mid-1;
-                answer = citations.length-mid;
-            } else {
-                left = mid+1;
+//             if(citations[mid]>=citations.length-mid) {
+//                 right = mid-1;
+//                 answer = citations.length-mid;
+//             } else {
+//                 left = mid+1;
+//             }
+//         }
+        
+        for (int i=0; i<citations.length; i++) {
+            if(citations[i]>=citations.length-i){ 
+                answer = citations.length-i;
+                break;
             }
-
         }
-            
+        
         return answer;
     }
 }
